26abecde5006ba4abfe342f476b8d668a9a8b20f
Mike AUBENAS <mike.aubenas@gmail.com>
2018-12-25 02:31:15 +0100

    WIP on setting the universe from JSON

--- README.md
+++ README.md
@@ -2,9 +2,7 @@
 
 ## TODO
 
-- [ ] Command line parser
 - [ ] Handling a default universe in a json file
 - [ ] Handling a default rules' set in a json file
 - [ ] Output every generation in a file prettily
-- [ ] User can choose how many generations
-- [ ] User can display the evolution of each generations in the CLI
+- [ ] Explain in the readme the syntax of the universe json file
--- src/Universe.js
+++ src/Universe.js
@@ -1,15 +1,37 @@
+const messageDisplayer = require( './messageDisplayer' );
+
 module.exports = class Universe
 {
 	constructor( jsonUniverse, jsonRules, howManyGenerations )
 	{
-		this.universe = this.validateJSON( jsonUniverse );
+		this.universe = this.setUniverse( jsonUniverse );
 	}
 
-	validateJSON( json )
+	setUniverse( json )
 	{
-		if ( Array.isArray( json[ 0 ] ) )
-			console.log( 'array' )
+		messageDisplayer.verbose( 'Setting universe ...' );
+
+		if ( Array.isArray( json ) )
+		{
+			let lengthFirstLine = json[ 0 ].length;
+
+			( function checkLinesSameLength()
+			{
+				for( let i = 0; i < json.length; i++ )
+				{
+					if ( json[ i ].length !== lengthFirstLine )
+						messageDisplayer.error( `Length of line ${ i } in your universe file is different from the prious one. Every line of your JSON should have the same length.` )
+				}
+			})();
+
+			( function checkCellsFormat()
+			{
+				// TODO Check if there's only 0 and 1 and maybe begin creating the data structure handling areas of interests in the map
+			})();
+
+			messageDisplayer.verbose( 'Universe set' );
+		}
 		else
-			console.log( 'not array' )
+			messageDisplayer.error( 'The universe JSON file you provided needs to be an array, please check out the README explaining it' )
 	}
 }
